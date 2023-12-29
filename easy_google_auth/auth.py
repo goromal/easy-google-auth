2c2ad8bd85c7ed26e81d2ffd9cb097336f8a5383
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-28 17:26:32 -0800

    allow creds to attempt to refresh in headless mode

--- easy_google_auth/auth.py
+++ easy_google_auth/auth.py
@@ -10,16 +10,18 @@ def getGoogleService(api_name, version, secrets_file, refresh_token, scope, head
     if os.path.exists(refresh_token):
         creds = Credentials.from_authorized_user_file(refresh_token, scope)
     if not creds or not creds.valid:
-        if headless:
-            return None
         if creds and creds.expired and creds.refresh_token:
             try:
                 creds.refresh(Request())
             except RefreshError:
+                if headless:
+                    return None
                 os.remove(refresh_token)
                 flow = InstalledAppFlow.from_client_secrets_file(secrets_file, scope)
                 creds = flow.run_local_server(port=0)
         else:
+            if headless:
+                return None
             flow = InstalledAppFlow.from_client_secrets_file(secrets_file, scope)
             creds = flow.run_local_server(port=0)
         with open(refresh_token, "w") as token:
