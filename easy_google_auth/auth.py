9c2195a712cd4ddf909c61324c9e707bf852313c
Andrew Torgesen <atorgesen@anduril.com>
2024-05-29 10:05:03 -0700

    Consolidate google secrets

--- easy_google_auth/auth.py
+++ easy_google_auth/auth.py
@@ -5,6 +5,13 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+_SCOPE = [
+    "https://mail.google.com/",
+    "https://www.googleapis.com/auth/documents",
+    "https://www.googleapis.com/auth/tasks",
+    "https://www.googleapis.com/auth/spreadsheets",
+]
+
 class CredentialsRefreshException(Exception):
     pass
 
@@ -16,23 +23,23 @@ def _refresh_creds(refresh_token, secrets_file, scope, headless):
     flow = InstalledAppFlow.from_client_secrets_file(secrets_file, scope)
     return flow.run_local_server(port=0)
 
-def getGoogleService(api_name, version, secrets_file, refresh_token, scope, headless=False, force=False):
+def getGoogleService(api_name, version, secrets_file, refresh_token, headless=False, force=False):
     creds = None
     if force:
-        creds = _refresh_creds(refresh_token, secrets_file, scope, headless)
+        creds = _refresh_creds(refresh_token, secrets_file, _SCOPE, headless)
         with open(refresh_token, "w") as token:
             token.write(creds.to_json())
     else:    
         if os.path.exists(refresh_token):
-            creds = Credentials.from_authorized_user_file(refresh_token, scope)
+            creds = Credentials.from_authorized_user_file(refresh_token, _SCOPE)
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 try:
                     creds.refresh(Request())
                 except RefreshError:
-                    creds = _refresh_creds(refresh_token, secrets_file, scope, headless)
+                    creds = _refresh_creds(refresh_token, secrets_file, _SCOPE, headless)
             else:
-                creds = _refresh_creds(refresh_token, secrets_file, scope, headless)
+                creds = _refresh_creds(refresh_token, secrets_file, _SCOPE, headless)
             with open(refresh_token, "w") as token:
                 token.write(creds.to_json())
     return build(api_name, version, credentials=creds)
