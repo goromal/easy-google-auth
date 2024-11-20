e83a1d94fd39e8c7e3dbc275e9c458a8909f992a
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-11-19 21:28:04 -0800

    wip

--- easy_google_auth/__version__.py
+++ easy_google_auth/__version__.py
@@ -4,7 +4,9 @@ __version__.py
 """
 
 __title__ = "easy_google_auth"
-__description__ = "Convenience library for abstracting away Google API authorization protocols"
+__description__ = (
+    "Convenience library for abstracting away Google API authorization protocols"
+)
 __version__ = "0.0.0"
 __author__ = "Andrew Torgesen"
 __author_email__ = "andrew.torgesen@gmail.com"
